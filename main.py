6d353c6a108b650ec9813e335c3cb48092cd9ccd
Zhu Zijun <2961163526@qq.com>
2023-02-06 02:50:52 +0800

    将微服务端口确定移交公共库配置 添加服务启动提示语句

--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import logging
 import grpc
 
 from micro_services_protobuf.mycqu_service import mycqu_service_pb2_grpc as ms_grpc
+from _321CQU.tools.gRPCManager import gRPCManager, ServiceEnum
 
-from utils.configManager import ConfigReader
 from servicers.MycquServicer import MycquServicer
 from servicers.CardServicer import CardServicer
 
 
 async def serve():
-    reader = ConfigReader()
-    port = reader.get_config('gRPCServiceConfig', 'MycquServicePort')
+    port = gRPCManager().get_service_config(ServiceEnum.MycquService)[1]
 
     server = grpc.aio.server()
     ms_grpc.add_MycquFetcherServicer_to_server(MycquServicer(), server)
@@ -22,5 +21,6 @@ async def serve():
 
 
 if __name__ == '__main__':
+    print("启动 mycqu service 服务")
     logging.basicConfig(level=logging.INFO)
     asyncio.new_event_loop().run_until_complete(serve())
