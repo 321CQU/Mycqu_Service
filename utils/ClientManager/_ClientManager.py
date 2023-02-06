aad7f978376dc4756cca0a878f1adda1d99bd646
Zhu Zijun <2961163526@qq.com>
2023-02-07 00:25:08 +0800

    ClientManager新增锁以避免数据竞争带来的重复请求问题

--- utils/ClientManager/_ClientManager.py
+++ utils/ClientManager/_ClientManager.py
@@ -17,22 +17,25 @@ class ClientManager:
     def __init__(self):
         self.clients: Dict[str, Dict[str, Tuple[AsyncClient, int]]] = {}
         reader = ConfigReader()
+        self.client_lock = asyncio.Lock()
         self.overtime_check_time = int(reader.get_config('ClientManager', 'overtime_check_time'))
         self.client_overtime = int(reader.get_config('ClientManager', 'client_overtime'))
-        asyncio.create_task(self.remove_overtime_client())
+        self.clean_task = asyncio.create_task(self.remove_overtime_client())
 
     async def acquire(self, description: str, id: str, login: Callable[[AsyncClient], Awaitable]) -> AsyncClient:
-        if description in self.clients.keys() and id in self.clients[description]:
-            return self.clients[description][id][0]
-        else:
-            return await self.launch_new_client(description, id, login)
+        async with self.client_lock:
+            if description in self.clients.keys() and id in self.clients[description]:
+                return self.clients[description][id][0]
+
+        return await self.launch_new_client(description, id, login)
 
     async def launch_new_client(self, description: str, id: str, login: Callable[[AsyncClient], Awaitable]) -> AsyncClient:
         client = AsyncClient(timeout=20)
         await login(client)
-        if description not in self.clients.keys():
-            self.clients[description] = {}
-        self.clients[description][id] = (client, int(time.time()))
+        async with self.client_lock:
+            if description not in self.clients.keys():
+                self.clients[description] = {}
+            self.clients[description][id] = (client, int(time.time()))
         return client
 
     async def remove_overtime_client(self):
@@ -43,17 +46,18 @@ class ClientManager:
         should_remove_path: List[Tuple[str, str]] = []
         should_remove_client: List[AsyncClient] = []
 
-        for description, pair in self.clients.items():
-            for id, value in pair.items():
-                if now - value[1] > self.client_overtime:
-                    should_remove_path.append((description, id))
-                    should_remove_client.append(value[0])
+        async with self.client_lock:
+            for description, pair in self.clients.items():
+                for id, value in pair.items():
+                    if now - value[1] > self.client_overtime:
+                        should_remove_path.append((description, id))
+                        should_remove_client.append(value[0])
 
-        async with asyncio.TaskGroup() as tg:
-            for client in should_remove_client:
-                tg.create_task(client.aclose())
+            async with asyncio.TaskGroup() as tg:
+                for client in should_remove_client:
+                    tg.create_task(client.aclose())
 
-        for description, id in should_remove_path:
-            self.clients[description].pop(id)
+            for description, id in should_remove_path:
+                self.clients[description].pop(id)
 
-        asyncio.create_task(self.remove_overtime_client())
+        self.clean_task = asyncio.create_task(self.remove_overtime_client())
